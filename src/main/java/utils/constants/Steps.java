c9e50584de64019766b7cbd26edf6894b04fe867
roman romadin <r.romadin2010@yandex.ru>
2024-06-16 21:58:55 +0300

    add User data + general data

---
+++ src/main/java/utils/constants/Steps.java
@@ -0,0 +1,10 @@
+package utils.constants;
+
+import api.OrderApi;
+import api.UserApi;
+
+public class Steps {
+    public final static UserApi USER_API = new UserApi();
+    public final static OrderApi ORDER_API = new OrderApi();
+
+}
